deeddd5ef8bf21ac452b72a057f166b1340c595c
moguimei <AllyCat88201@gmail.com>
2023-02-25 21:29:44 -0500

    commit 2
    part 1 vid

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerScript : MonoBehaviour
 {
+    private Rigidbody2D rd2d;
+
+    public float speed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rd2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        float hozMovement = Input.GetAxis("Horizontal");
+        float vertMovement = Input.GetAxis("Vertical");
+        rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
         
     }
 }
